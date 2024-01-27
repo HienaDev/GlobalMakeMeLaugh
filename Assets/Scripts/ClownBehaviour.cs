c2b3628bb538ed80e6dbcf6eeca4d2d3b397e930
HienaDev <ant.rodrigues1999@gmail.com>
2024-01-27 15:49:51 +0000

    Add banana fall and create player and clown prefabs

--- Assets/Scripts/ClownBehaviour.cs
+++ Assets/Scripts/ClownBehaviour.cs
@@ -58,7 +58,7 @@ public class ClownBehaviour : MonoBehaviour
 
 
     public void Shake(){
-        StartCoroutine(cameraShake.Shake(.5f, 1f));
+        //StartCoroutine(cameraShake.Shake(.5f, 1f));
     }
 
     public bool IsChasing(){
