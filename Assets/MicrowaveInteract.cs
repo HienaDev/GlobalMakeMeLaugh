751aebe4d2bbb2b30a345ae443999d29a56d5f60
HienaDev <ant.rodrigues1999@gmail.com>
2024-01-28 16:27:23 +0000

    idk

--- Assets/MicrowaveInteract.cs
+++ Assets/MicrowaveInteract.cs
@@ -65,7 +65,7 @@ public class MicrowaveInteract : MonoBehaviour
         //Iinicio de corrotina, micro ondas fica ligado
         animator.SetBool("On", true);
 
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(10f);
 
         animator.SetBool("On", false);
 
